550e490dcde7309581848e381dfa53a961095c8e
VedavyasBhat <vedavyas.bhat93@gmail.com>
2015-07-05 02:49:30 +0530

    Minor testing changes and added readme

--- README.md
+++
@@ -1,3 +0,0 @@
-# Apriori
-
-A naive implementation of the Apriori algorithm. Reads .arff files.
--- src/Test.java
+++ src/Test.java
@@ -5,25 +5,25 @@ import DataStructures.Database;
 public class Test {
     //static String file = "/home/vedavyas/project/weka-3-6-12/data/weather.nominal.arff";
     //static String file = "/home/vedavyas/project/data/heart/cleveland/clev_nominalized.arff";
-    static String file = "/home/vedavyas/project/data/heart/cleveland/initial/data/clev_0_1.arff";
+    //static String file = "/home/vedavyas/project/data/heart/cleveland/initial/data/clev_0_1.arff";
     //static String file = "/home/vedavyas/project/data/heart/cleveland/nominalized/clev_nominalized.arff";
     //static String file = "/home/vedavyas/project/data/heart/hungarian/hungarian_nominalized.arff";
     //static String file = "/home/vedavyas/project/Data/TestData.arff";
     
-    public static void testDatabase() throws Exception {
+    public static void testDatabase(String file) throws Exception {
         Database database = new Database(file);
         System.out.println(database);
     }
     
-    public static void testApriori() throws Exception {
-        Database database = new Database(file);
+    public static void testApriori(String arffFile) throws Exception {
+        Database database = new Database(arffFile);
         Apriori apriori = new Apriori(database, 0.15f, 90f, true);
         apriori.runApriori();
     }
     
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        testApriori();
+        testApriori(args[0]);
         long end = System.currentTimeMillis();
         System.out.println("\n\n"+(end-start)+"ms");
     }
